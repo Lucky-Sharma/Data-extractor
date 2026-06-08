f344091900e182a0437a7a2ac2a6a88b2cd30dd6
Lucky-Sharma <Luckysharmab1@gmail.com>
2026-06-09 01:23:59 +0530

    working on integration

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,27 @@
-console.log("Hello via Bun!");
+import { tavily } from '@tavily/core'
 import express from "express";
 
+const client = tavily({ apiKey: process.env.TAVILY_API_KEY });
 const app = express();
 
-app.post("/conversation", async (req, res) => {
-    //
+app.use(express.json());
+
+app.post("/ask", async (req, res) => {
+    //get the query from the user 
+    const prompt = req.body.query;
+
+
+    //check the user have enough credit 
+    //check if we have web search indexed for similar query  
+    //web search to gether sources
+
+    const webSearch = await client.search(prompt, {
+        searchDepth: "advanced"
+    })
+    const webSearchResult = webSearch.results;
+    //hit the llm and stram back the response 
+
+
 })
 
 app.listen(3000);
