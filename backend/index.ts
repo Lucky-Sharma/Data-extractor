3f9dd580fbd69a20d7b8f3355594994f3a859e52
LUCKY SHARMA <26050027@mponline.demo.gov.in>
2026-06-09 18:05:17 +0530

    api integration

--- backend/index.ts
+++ backend/index.ts
@@ -1,25 +1,53 @@
 import { tavily } from '@tavily/core'
+import { createAiGateway } from "ai-gateway-provider";
+import { createUnified } from "ai-gateway-provider/providers/unified";
+import { generateObject } from "ai";
 import express from "express";
+import { PROMPT_TEMPLATE } from './prompt';
+import {z} from "zod";
 
 const client = tavily({ apiKey: process.env.TAVILY_API_KEY });
 const app = express();
 
 app.use(express.json());
 
+const SearchResponseSchema = z.object({
+    answer:z.string(),
+    summary:z.string(),
+    keyPoints:z.array(z.string()),
+})
+
 app.post("/ask", async (req, res) => {
     //get the query from the user 
-    const prompt = req.body.query;
+    const query = req.body.query;
 
 
     //check the user have enough credit 
     //check if we have web search indexed for similar query  
     //web search to gether sources
 
-    const webSearch = await client.search(prompt, {
+    const webSearch = await client.search(query, {
         searchDepth: "advanced"
     })
     const webSearchResult = webSearch.results;
+    
     //hit the llm and stram back the response 
+    const aigateway = createAiGateway({
+     accountId: "b4e5ce498746a4587131d73bb0ba2251",
+     gateway: "default",
+    apiKey: process.env.CF_AIG_TOKEN,
+    });
+
+    const unified = createUnified();
+
+    const prompt = PROMPT_TEMPLATE
+    .replace("{{WEB_SEARCH_RESULTS}}",JSON.stringify(webSearchResult))
+    .replace("{{USER_QUERY}}",query)
+
+    const { text } = await generateText({
+    model: aigateway(unified("workers-ai/@cf/meta/llama-3.3-70b-instruct-fp8-fast")),
+    prompt: prompt,
+    });
 
 
 })
--- backend/prompt.ts
+++ backend/prompt.ts
@@ -1,4 +1,4 @@
-const SYSTEM_PROMPT = `You are an expert assistent called cortana .
+export const SYSTEM_PROMPT = `You are an expert assistent called cortana .
 you job is to simple ,given the USER_QUERY and a bunch of web responses,
 try to answer the user query to the best of your abilities. YOU DONT HAVE 
 ACCESS TO ANY TOOLS.you are being given all the context that is needed to 
